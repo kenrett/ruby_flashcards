4a904f213285dbb0114ed60b5753d33cafa5034b
Vincent Storme <storme.vincent@gmail.com>
2013-04-12 15:43:58 -0700

    updated reader_writer

--- reader_writer.rb
+++ reader_writer.rb
@@ -12,17 +12,6 @@ class ReaderWriter
     lines.each_index { |i| qa << [lines[i], lines[i+1]] if i.even? }
     qa.each { |card| flashcards << Flashcard.new(card) }
 
-<<<<<<< HEAD
-puts ReaderWriter.read.inspect
-  def initialize(input)
-    @filename = input
-    parse_data(@filename)
-  end
-
-  def parse_data(file)
-    FILE.open(file) do |item|
-=======
     flashcards 
   end
->>>>>>> reader_writer
 end
