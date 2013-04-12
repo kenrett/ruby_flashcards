21a3ce307bab728060332ffa56cff1d0b3f410ed
Sam + Sida <apprentice@dbc08.local>
2013-04-12 14:51:32 -0700

    ReaderWriter v.0.1

---
+++ reader_writer.rb
@@ -0,0 +1,21 @@
+class ReaderWriter
+  def self.read
+    lines = []
+    flashcards = []
+    File.foreach('flashcard_sample.txt') do |line| 
+      lines << line if line != "\n"
+    end
+
+    flashcards.each_with_index do |line, i|
+      if line.odd?
+        question = line[i]
+      else
+        answer = line[i]
+      end
+      flashcards << Flashcard.new(question, answer)
+    end 
+    flashcards 
+  end
+end
+
+puts ReaderWriter.read.inspect
