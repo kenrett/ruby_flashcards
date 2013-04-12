2cab681bf766484e1303c43767a8eff7e3f570f4
Apprentice <apprentice@dbc07.local>
2013-04-12 14:33:37 -0700

    started interface

---
+++ interface.rb
@@ -0,0 +1,24 @@
+class Interface
+
+  def initialize(filename)
+    @all_flashcards =[]
+    load(filename)
+  end
+
+  def load(filename)
+    ReaderWriter.load # <=== check to see what they're calling it...
+      end
+
+  def guess
+
+  end
+
+  def validate
+
+  end
+
+  def shuffle
+
+  end
+
+end
